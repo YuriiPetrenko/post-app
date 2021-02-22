5ebc240ac44952c36c956783acd67c021b664881
yuriipetrenko <obzora6@gmail.com>
2021-02-22 05:33:51 +0100

    initial commit add components and styles

---
+++ src/components/post-add-form/post-add-form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './post-add-form.css';
+
+const PostAddForm = () => {
+         return (
+                  <form className="bottom-panel d-flex">
+                           <input
+                           type="text"
+                           placeholder="Write something"
+                           className="form-control new-post-label"
+                           />
+                           <button 
+                           type="submit"
+                           className="btn btn-outline-socondary">
+                           Add</button>
+                  </form>
+         )
+}
+
+export default PostAddForm;
