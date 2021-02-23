a5da5606f08fd0e11b1ceb2fe53c657c3ba08991
yuriipetrenko <obzora6@gmail.com>
2021-02-23 22:05:08 +0100

    add search

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './post-add-form.css';
 
-const PostAddForm = ({onAdd}) => {
-         return (
-                  <div className="bottom-panel d-flex">
-                           <input
-                           type="text"
-                           placeholder="Write something"
-                           className="form-control new-post-label"
-                           />
-                           <button 
-                           type="submit"
-                           className="btn btn-outline-socondary"
-                           onClick={()=>onAdd('Added')}
+export default class PostAddForm extends Component {
+         constructor(props){
+                  super(props);
+                  this.state = {
+                           text: ''
+                  }
+                  this.onValueChange = this.onValueChange.bind(this);
+                  this.onSubmit = this.onSubmit.bind(this);
+         }
+
+         onValueChange(e){
+                  this.setState({
+                           text: e.target.value
+                  })
+         }
+
+         onSubmit(e){
+                  e.preventDefault();
+                  this.props.onAdd(this.state.text)
+                  this.setState({
+                           text: ''
+                  })
+         }
+
+         render() {
+                  return (
+                           <form 
+                           className="bottom-panel d-flex"
+                           onSubmit={this.onSubmit}
                            >
-                           Add</button>
-                  </div>
-         )
+                                    <input
+                                    type="text"
+                                    placeholder="Write something"
+                                    className="form-control new-post-label"
+                                    onChange={this.onValueChange}
+                                    value={this.state.text}
+                                    />
+                                    <button 
+                                    type="submit"
+                                    className="btn btn-outline-socondary"
+                                    >
+                                    Add</button>
+                           </form>
+                  )
+         }
 }
 
-export default PostAddForm;
